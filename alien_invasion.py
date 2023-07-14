65eb20caca11556955943b932b3d464003db13da
Cesar Plasencia Jr <codeberg@cesarp.dev>
2023-07-13 19:44:50 -0700

    Initial commit

---
+++ alien_invasion.py
@@ -0,0 +1,50 @@
+import sys
+import pygame
+
+from settings import Settings
+from ship import Ship
+
+class AlienInvasion:
+    """Overall class to manage game assets and behavior."""
+
+    def __init__(self):
+        """Initialize the game, and create game resources."""
+        pygame.init()
+
+        # Define the clock.
+        self.clock = pygame.time.Clock()
+
+        self.settings = Settings()
+
+        # Set the screen resolution to 1200 x 800.
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
+
+        pygame.display.set_caption("Alien Invasion!")
+
+        self.ship = Ship(self)
+
+    def run_game(self):
+        """Start the main loop for the game."""
+        while True:
+            # Watch for keyboard and mouse events.
+            for event in pygame.event.get():
+                # Exit the game if the game's close window button is clicked.
+                if event.type == pygame.QUIT:
+                    sys.exit()
+
+            # Redraw the screen during each pass through the loop.
+            self.screen.fill(self.settings.bg_color)
+
+            # Draw the ship.
+            self.ship.blitme()
+
+            # Draw new frame.
+            pygame.display.flip()
+
+            # Set game to run at 60 FPS.
+            self.clock.tick(60)
+
+if __name__ == '__main__':
+    # Make a game instance, and run the game.
+    ai = AlienInvasion()
+    ai.run_game()
