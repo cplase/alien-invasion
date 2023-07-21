155670ba2ced4c1487ab4f7192ebaf913eed0b53
Cesar Plasencia <54040184+cplase@users.noreply.github.com>
2023-07-21 15:56:59 -0700

    Implement fullscreen mode
    Configure the game to run in fullscreen mode.

--- alien_invasion.py
+++ alien_invasion.py
@@ -16,8 +16,10 @@ class AlienInvasion:
 
         self.settings = Settings()
 
-        # Set the screen resolution to 1200 x 800.
-        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
+        # Set the screen resolution to fullscreen.
+        self.screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
+        self.settings.screen_width = self.screen.get_rect().width
+        self.settings.screen_height = self.screen.get_rect().height
 
         pygame.display.set_caption("Alien Invasion!")
 
